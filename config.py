86377bac7a800a6a1726d7d60cecfb9bce46a2a3
usaspy <admin@example.com>
2022-02-14 13:18:27 +0800

    初始化

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ RESTFul_API_HOST = "0.0.0.0"
 RESTFul_API_PORT = 5550
 
 #小程序信息
-APPID = "wx3eb56946f427e979"
-APPSECRET = "efaf9c63776a12807e9dcef29987e138"
+APPID = "wxb03ae445308cbf31"
+APPSECRET = "ab459b1c3ffc2c133a09c62a30b3c985"
 
 #本应用信息
 SYSTEM_ID = "restapi-staff-01"
