2e31bb5f82347a6657f1f5c638937eb8d29dbe30
usaspy <admin@example.com>
2022-02-25 14:10:25 +0800

    初始化

--- api/orderApi.py
+++ api/orderApi.py
@@ -23,7 +23,7 @@ def in_confirm(order_id):
       return jsonify({"result": "failure", "reason":"入场确认出现异常"})
 
 
-#预约单状态变更-车辆离场-人工收费
+#预约单状态变更-场内车辆-离场-显示费用
 @app.route('/api/staff/pay_manual/<string:order_id>', methods=['PUT'])
 def pay_manual(order_id):
    #token_info = tools.getTokenInfo(request,TOKENS_CACHE)
@@ -32,7 +32,7 @@ def pay_manual(order_id):
 
    return jsonify({"vehicle_number":vehicle.vehicle_number, "fee_1": str(bill.fee_1),"start_time":bill.start_time,"end_time":bill.end_time,"duration":bill.duration,"pay_state":bill.pay_state})
 
-#预约单状态变更-车辆离场-人工确认放行
+#预约单状态变更-场内车辆-离场-人工收费后放行
 @app.route('/api/staff/out_confirm/<string:order_id>',methods=['PUT'])
 def out_confirm(order_id):
    token_info = tools.getTokenInfo(request,TOKENS_CACHE)
